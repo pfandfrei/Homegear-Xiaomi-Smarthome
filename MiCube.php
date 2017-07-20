60b97bf3b45c30a74f14622b33ce7e46d5f832ef
Frank Motzkau <frank@motzkau.de>
2017-07-20 21:09:57 +0200

    bugfix cube controller

--- MiCube.php
+++ MiCube.php
@@ -9,7 +9,7 @@ include_once 'MiConstants.php';
 
 class MiCube extends MiBaseDevice
 {
-    const TYPE_ID = 0x287c;
+    const TYPE_ID = 0x28bc;
     
     public function __construct($config)
     {
@@ -17,55 +17,65 @@ class MiCube extends MiBaseDevice
         parent::__construct($config);        
     }
     
-    public function getTypeId() { return MiSwitch::TYPE_ID; }
+    public function getTypeId() { return MiCube::TYPE_ID; }
     
     public function updateData($hg, $data)
     {
         parent::updateData($hg, $data);
         
-        if (property_exists($data, 'status'))
+        if (property_exists($data, 'rotate'))
+        {
+            $rotate = json_decode($data->rotate);
+            list($angle, $time) = split(',', $rotate);
+            $hg->setValue($this->_peerId, 2, 'ROTATE', intval($angle));
+            $hg->setValue($this->_peerId, 2, 'TIME', intval($time));
+            if (intval($angle) < 0)
             {
-                $status = json_decode($data->status);
-                switch ($status)
-                {
-                    case 'flip90':
-                        $hg->setValue($this->_peerId, 1, 'FLIP90', TRUE);
-                        break;
-                    case 'flip180':
-                        $hg->setValue($this->_peerId, 1, 'FLIP180', TRUE);
-                        break;
-                    case 'move':
-                        $hg->setValue($this->_peerId, 1, 'MOVE', TRUE);
-                        break;
-                    case 'tap_twice':
-                        $hg->setValue($this->_peerId, 1, 'TAP_TWICE', TRUE);
-                        break;
-                    case 'shake_air':
-                        $hg->setValue($this->_peerId, 1, 'SHAKE_AIR', TRUE);
-                        break;
-                    case 'swing':
-                        $hg->setValue($this->_peerId, 1, 'SWING', TRUE);
-                        break;
-                    case 'alert':
-                        $hg->setValue($this->_peerId, 1, 'SHAKE_AIR', TRUE);
-                        break;
-                    case 'free_fall':
-                        $hg->setValue($this->_peerId, 1, 'SWING', TRUE);
-                        break;
-                    case 'rotate_right':
-                        $hg->setValue($this->_peerId, 1, 'ROTATE_RIGHT', TRUE);
-                        break;
-                    case 'rotate_left':
-                        $hg->setValue($this->_peerId, 1, 'ROTATE_LEFT', TRUE);
-                        break;
-                    default:
-                        break;
-                }
+                $hg->setValue($this->_peerId, 2, 'ROTATE_LEFT', TRUE);
             }
+            else
+            {
+                $hg->setValue($this->_peerId, 2, 'ROTATE_RIGHT', TRUE);                
+            }
+        }
+        if (property_exists($data, 'status'))
+        {
+            $status = json_decode($data->status);
+            switch ($status)
+            {
+                case 'flip90':
+                    $hg->setValue($this->_peerId, 1, 'FLIP90', TRUE);
+                    break;
+                case 'flip180':
+                    $hg->setValue($this->_peerId, 1, 'FLIP180', TRUE);
+                    break;
+                case 'move':
+                    $hg->setValue($this->_peerId, 1, 'MOVE', TRUE);
+                    break;
+                case 'tap_twice':
+                    $hg->setValue($this->_peerId, 1, 'TAP_TWICE', TRUE);
+                    break;
+                case 'shake_air':
+                    $hg->setValue($this->_peerId, 1, 'SHAKE_AIR', TRUE);
+                    break;
+                case 'swing':
+                    $hg->setValue($this->_peerId, 1, 'SWING', TRUE);
+                    break;
+                case 'alert':
+                    $hg->setValue($this->_peerId, 1, 'SHAKE_AIR', TRUE);
+                    break;
+                case 'free_fall':
+                    $hg->setValue($this->_peerId, 1, 'SWING', TRUE);
+                    break;
+                default:
+                    break;
+            }
+        }
     }
     
     public function updateEvent($hg, $event)
     {
         parent::updateEvent($hg, $event);
+        return FALSE;
     }
 }
