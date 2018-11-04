2f10f6bd94b0599a457e44787958f2eb199811e5
Frank Motzkau <frank@motzkau.de>
2018-11-04 11:14:44 +0100

    add install script

--- install.sh
+++ install.sh
@@ -7,8 +7,7 @@ fi
 HOMEGEAR_DIR=/var/lib/homegear/scripts/DeviceScripts/Xiaomi
 SCRIPT_DIR="$( cd "$(dirname $0)" && pwd )"
 
-cp "$SCRIPT_DIR/Mi*.xml" /etc/homegear/devices/254/
+cp ${SCRIPT_DIR}/Mi*.xml /etc/homegear/devices/254/
 
-mkdir -p "$HOMEGEAR_DIR"
-cp "$SCRIPT_DIR/Mi*.php" "$HOMEGEAR_DIR"
-chown -R homegear:homegear "$HOMEGEAR_DIR"
+mkdir -p ${HOMEGEAR_DIR}
+cp ${SCRIPT_DIR}/Mi*.php ${HOMEGEAR_DIR}
